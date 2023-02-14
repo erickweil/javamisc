6e9575f4ec468e5c585ec33411de2e48c3fcc816
Erick Weil <erick.weil@ifro.edu.br>
2023-02-13 23:35:38 -0300

    Adicionado tempo

--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -15,10 +15,17 @@ public class Sorting {
      */
     public static void main(String[] args) {
         //                        0 1 2 3 4 5 6 7 8 9 
-        int[] numeros = new int[]{1, 57, 38, 30, 90, 17, 87, 16, 89, 76};
+        int[] numeros = new int[40000];
+        for(int k =0;k < numeros.length; k++)
+        {
+            numeros[k] = numeros.length - k;
+        }
         
         System.out.print("Antes:\t");
         printar(numeros);
+        
+        
+        long tempoInicio = System.nanoTime();
         
         int ntrocas;
         do {
@@ -42,8 +49,15 @@ public class Sorting {
         }
         while(ntrocas > 0);
         
+        long tempoFim = System.nanoTime();
+        
         System.out.print("Depois:\t");
         printar(numeros);
+        
+        double tempoDiff = (tempoFim - tempoInicio)/1000000.0;
+        System.out.printf("Tempo de Execução: %.2f milissegundos \n",tempoDiff);
+        
+        
     }
     
     public static void printar(int[] arr) {
